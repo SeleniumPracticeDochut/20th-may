389203ed7ebab2932f7fe345af8444664eac75a6
SeleniumPracticeDochut <darshanlakshminarayana@gmail.com>
2026-05-19 17:59:24 +0530

    second change

--- tests/example.spec.js
+++ tests/example.spec.js
@@ -6,6 +6,9 @@ test('has title', async ({ page }) => {
 
   // Expect a title "to contain" a substring.
   await expect(page).toHaveTitle(/Playwright/);
+  console.log("hello")
+    console.log("hello")
+
 });
 
 test('get started link', async ({ page }) => {
